e30c925e4d3ea18641b0aaa87983069d13a6e764
sabinbogati <sabin.bogati@convergetree.com>
2018-09-30 19:31:32 +0545

    Like, Unlike Api Routes - POST

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -77,6 +77,67 @@ router.post("/", passport.authenticate('jwt',{session:false}), (req,res)=>{
 })
 
 
+
+//@route POST api/posts/like/:id
+//@desc Like post
+//@access private
+
+router.post("/like/:id", passport.authenticate('jwt', {session:false}), (req,res)=>{
+
+    Profile.findOne({user: req.user.id})
+        .then(profile =>{
+            Post.findById(req.params.id)
+                .then(post =>{
+                        if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
+                                return res.status(400).json({alreadyliked : "user already liked this post"});
+                        }
+                        else{
+
+                            //Add user id to likes array
+                            post.likes.unshift({ user: req.user.id});
+
+                            post.save().then(post => res.json(post))
+                        }
+                })
+                .catch(err=> res.status((404).json({postnotfound : "No post found"})));
+        });
+})
+
+
+
+//@route POST api/posts/unlike/:id
+//@desc UnLike post
+//@access private
+
+router.post("/unlike/:id", passport.authenticate('jwt', {session:false}), (req,res)=>{
+
+    Profile.findOne({user: req.user.id})
+        .then(profile =>{
+            Post.findById(req.params.id)
+                .then(post =>{
+                    if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0){
+                        return res.status(400).json({notliked : "you have not yet liked this post"});
+                    }
+                    else{
+
+                        //Get remove index
+
+                        const removeIndex = post.likes.map(item => item.user.toString()).indexOf(req.user.id);
+
+                        //Splice out of
+                        post.likes.splice(removeIndex,1);
+
+                        //Save
+                        post.save().then(post => res.json(post));
+
+                    }
+                })
+                .catch(err=> res.status((404).json({postnotfound : "No post found"})));
+        });
+})
+
+
+
 //@route DELETE api/posts/:id
 //@desc DELETE post
 //@access private
