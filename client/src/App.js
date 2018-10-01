71dd3a4d7c09ee0af22766215217698950e88f6b
sabinbogati <sabin.bogati@convergetree.com>
2018-10-01 08:49:28 +0545

    React Router v4 setup...

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 
+import './App.css';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <Navbar/>
-                   <Landing/>
-          <Footer/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Navbar/>
+                        <Route exact path="/" component={Landing}/>
+                    <Footer/>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
