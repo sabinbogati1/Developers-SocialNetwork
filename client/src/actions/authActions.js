ff6527d456df1380949c0e980801078e75d362ef
sabinbogati <sabin.bogati@convergetree.com>
2018-10-02 15:30:07 +0545

    Redux Login Action Set Current User...

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
-import {GET_ERRORS} from './types';
 import axios from "axios";
+
+import setAuthToken from '../utils/setAuthToken';
+import {GET_ERRORS, SET_CURRENT_USER} from './types';
 
 //Register User
 //Normal way
@@ -26,4 +28,43 @@ export  const registeruser = (userData, history) =>dispatch =>{
         );
 
 }
+
+
+//Login - Get User Token
+export const loginUser = userData => dispatch =>{
+    axios.post('/api/users/login', userData )
+        .then(res =>{
+
+            //Save to localStorage
+            const {token} = res.data;
+
+            //Set token to ls
+            localStorage.setItem('jwtToken', token);
+
+            //Set token to Auth header
+            setAuthToken(token);
+
+            //Decode token to get user data
+            const decoded = jwt_decode(token);
+
+            //Set Current User
+            dispatch(setCurrentUser(decoded));
+
+        }).catch(err => dispatch({
+        type: GET_ERRORS,
+        payload : err.response.data
+    }))
+}
+
+
+//Set logged in user
+export const setCurrentUser = (decoded) =>{
+    return{
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
+}
+
+
+
 
